ab3b78219b9961e7b1fe3fae26645880b3bc152f
Shivam <152253499+shivamsoni-in@users.noreply.github.com>
2023-12-04 12:47:12 +0000

    conversion of datatypes

---
+++ 01_jsbasic/03_conversion.js
@@ -0,0 +1,35 @@
+// let age = "24shivam"
+// let age = null
+// let age = undefined
+// let age = true
+// let age = false
+let age = "shiva"
+
+// console.log(typeof age);
+// console.log(typeof(age));
+
+let valueNum = Number(age)
+// console.log(typeof valueNum);
+// console.log(valueNum);
+
+
+// "24" => 24
+// "24shivam" => NaN
+//  true => 1; false => 0
+
+
+
+let isLoggged = "shivaa"
+
+let booleanCheck = Boolean(isLoggged)
+// console.log(booleanCheck);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let sumNum = 24
+let sumNumStr = String(sumNum);
+console.log(typeof sumNumStr);
+console.log(sumNumStr);
