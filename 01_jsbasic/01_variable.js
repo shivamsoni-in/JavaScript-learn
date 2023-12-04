6ee9331f7fae5e58d3f266c21d94cdd370c871b5
Shivam <152253499+shivamsoni-in@users.noreply.github.com>
2023-12-04 10:07:14 +0000

    container config done

---
+++ 01_jsbasic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 65453
+let accountName = "Shiv"
+var accountPass = "github"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 55555
+accountName = "vinayak"
+accountPass = "gut"
+accountCity = "pune"
+
+// console.log(accountId);
+console.table([accountId, accountName, accountPass, accountCity, accountState])
