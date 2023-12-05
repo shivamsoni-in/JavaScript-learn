b9a3f338073c8f95118fb1bbe539cea940c38ec4
Shivam <152253499+shivamsoni-in@users.noreply.github.com>
2023-12-05 13:16:16 +0000

    dataTypes Summey

---
+++ 01_jsbasic/dataTypes_summery.js
@@ -0,0 +1,10 @@
+const id = Symbol(346)
+const newId = Symbol(125)
+
+console.log(typeof id);
+
+
+const bigNumber = 654654651654218465n
+
+// console.log(typeof bigNumber);
+// console.log(bigNumber);
